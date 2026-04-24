9a2806ad809abe81459479be630a386f15c33b5d
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-04-24 15:46:41 -0400

    Project initialised

---
+++ src/test/java/io/github/jaswinder/distributedlockdemo/DistributedLockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaswinder.distributedlockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
